3b846314d7e4e6c5daa23b60d30b24646ee3bd47
Marina <climovicimarina@gmail.com>
2024-03-26 16:37:09 +0200

    added normalize method and configured unittests

--- problems/ratio/ratio.py
+++ problems/ratio/ratio.py
@@ -10,15 +10,17 @@ class Ratio:
     def __init__(self, numerator: int, denominator: int):
         self.numer = numerator
         self.denom = denominator
-        self.gcd = 1
+
+        if self.denom == 0 or self.denom < 0:
+            raise ValueError("Denominator cannot be equal to 0 or negative")
 
     def add(self, other: 'Ratio') -> 'Ratio':
         ''' add the rational numbers '''
         common_denom = self.denom * other.denom
         new_numer = self.numer * other.denom + other.numer * self.denom
 
-        gcd_value = self.greatest_common_divisor(new_numer, common_denom)
-        return Ratio(new_numer//gcd_value, common_denom//gcd_value)
+        new_numer, common_denom = self.normalize(new_numer, common_denom)
+        return Ratio(new_numer, common_denom)
 
     def sub(self, other: 'Ratio') -> 'Ratio':
         ''' subtract the rational numbers '''
@@ -26,8 +28,8 @@ class Ratio:
         common_denom = self.denom * other.denom
         new_numer = self.numer * other.denom - other.numer*self.denom
 
-        gcd_value = self.greatest_common_divisor(new_numer, common_denom)
-        return Ratio(new_numer//gcd_value, common_denom//gcd_value)
+        new_numer, common_denom = self.normalize(new_numer, common_denom)
+        return Ratio(new_numer, common_denom)
 
     def mul(self, other: 'Ratio') -> 'Ratio':
         ''' multiply the rational numbers '''
@@ -35,8 +37,8 @@ class Ratio:
         multiplied_numer = self.numer * other.numer
         multiplied_denom = self.denom * other.denom
 
-        gcd_value = self.greatest_common_divisor(multiplied_numer, multiplied_denom)
-        return Ratio(multiplied_numer//gcd_value, multiplied_denom//gcd_value)
+        multiplied_numer, multiplied_denom = self.normalize(multiplied_numer, multiplied_denom)
+        return Ratio(multiplied_numer, multiplied_denom)
 
     def div(self, other: 'Ratio') -> 'Ratio':
         ''' divide the rational numbers '''
@@ -44,16 +46,23 @@ class Ratio:
         divided_numer = self.numer * other.denom
         divided_denom = self.denom * other.numer
 
-        gcd_value = self.greatest_common_divisor(divided_numer, divided_denom)
-        return Ratio(divided_numer//gcd_value, divided_denom//gcd_value)
+        divided_numer, divided_denom = self.normalize(divided_numer, divided_denom)
+        return Ratio(divided_numer, divided_denom)
 
-    def greatest_common_divisor(self, numerator: int, denominator: int) -> int:
-        ''' find greatest common divisor '''
+    def normalize(self, numerator: int, denominator: int) -> tuple[int, int]:
+        ''' find greatest common divisor and eliminate '-' if bothe numer and denom are negative'''
+        gcd = 1
 
+        if numerator < 0 and denominator < 0:
+            numerator = abs(numerator)
+            denominator = abs(denominator)
+        
         for i in range(1, min(abs(numerator), abs(denominator)) + 1):
             if numerator % i == 0 and denominator % i == 0:
-                self.gcd = i
-        return self.gcd
+                gcd = i
+        normalized_numer = numerator//gcd
+        normalized_denom = denominator//gcd
+        return normalized_numer, normalized_denom
 
     def to_float(self) -> float:
         ''' convert the rational number to float '''
@@ -73,4 +82,4 @@ if __name__ == "__main__":
     print("mul result = ", x.mul(y).to_string())
     print("div result = ", x.div(y).to_string())
     print(x.to_string(), x.to_float())
-    assert x.to_string() == "-3/8"
+    assert(x.to_string() == "-3/8")
--- problems/ratio/tests.py
+++ problems/ratio/tests.py
@@ -1,7 +1,69 @@
 import unittest
 
-from ratio import add, sub, mul, div, greatest_common_diviser, to_float, to_string
+from ratio import Ratio
 
-class TestClass(unittest.TestCase):
+class TestRatio(unittest.TestCase):
+    '''test class Ratio'''
+    def setUp(self):
+        self.ratio = Ratio(numerator=-4, denominator=9)
+        self.different_denom = Ratio(numerator=6, denominator=7)
+        self.equal_denom = Ratio(numerator=5, denominator=9)
+        self.negative_numer = Ratio(numerator=-7, denominator=12)
+    
     def test_0_add(self):
-        self.assertEqual()
+        '''different denominators: test add method'''
+        result = self.ratio.add(self.different_denom).to_string()
+        self.assertEqual(result, "26/63")
+
+    def test_1_add(self):
+        '''equal denominators: test add method'''
+        result = self.ratio.add(self.equal_denom).to_string()
+        self.assertEqual(result, "1/9")
+
+    def test_2_add(self):
+        '''negative numerators: test add method'''
+        result = self.ratio.add(self.negative_numer).to_string()
+        self.assertEqual(result, "-37/36")
+
+    def test_0_sub(self):
+        '''different denominators: test sub method'''
+        result = self.ratio.sub(self.different_denom).to_string()
+        self.assertEqual(result, "-82/63")
+
+    def test_1_sub(self):
+        '''equal denominators: test sub method'''
+        result = self.ratio.sub(self.equal_denom).to_string()
+        self.assertEqual(result, "-1/1")
+
+    def test_2_sub(self):
+        '''negative numerators: test sub method'''
+        result = self.ratio.sub(self.negative_numer).to_string()
+        self.assertEqual(result, "5/36")
+
+    def test_0_mul(self):
+        '''different denominators: test mul method'''
+        result = self.ratio.mul(self.different_denom).to_string()
+        self.assertEqual(result, "-8/21")
+
+    def test_1_mul(self):
+        '''negative numerators: test mul method'''
+        result = self.ratio.mul(self.negative_numer).to_string()
+        self.assertEqual(result, "7/27")
+
+    def test_0_div(self):
+        '''negative numerators: test div method'''
+        result = self.ratio.div(self.negative_numer).to_string()
+        self.assertEqual(result, "16/21")
+
+    def test_1_div(self):
+        '''different denumerators: test div method'''
+        result = self.ratio.div(self.different_denom).to_string()
+        self.assertEqual(result, "-14/27")
+
+    def test_1_to_float(self):
+        '''convert rational number to float'''
+        result = self.ratio.to_float()
+        self.assertEqual(result, -0.4444444444444444)
+
+if __name__ == '__main__':
+    unittest.main()
