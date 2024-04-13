86acbad1aa9c6625a347013213a6c0d31d06106f
Marina <climovicimarina@gmail.com>
2024-04-13 10:40:42 +0200

    added draw methods and dataclass Point

--- problems/oop_shapes/shapes.py
+++ problems/oop_shapes/shapes.py
@@ -1,4 +1,7 @@
 import math
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
+from tkinter import Canvas, Tk, mainloop
 
 def distance(point1: tuple, point2: tuple):
     '''Calculate the distance between two points in a 2-dimensional plane.'''
@@ -6,54 +9,70 @@ def distance(point1: tuple, point2: tuple):
     x2, y2 = point2
     return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
+@dataclass
+class Point():
+    '''2D point'''
+    x: float
+    y: float
 
-class Shape:
-    '''superclass'''
+class Shape(ABC):
+    '''abstract shape'''
+    @abstractmethod
     def name(self) -> str:
         '''shape's name'''
-        raise NotImplementedError()
-    
+
+    @abstractmethod
     def area(self) -> float:
         '''shape's area'''
-        raise NotImplementedError()
 
-    def center(self) -> tuple:
+    @abstractmethod
+    def center(self) -> Point:
         '''shape's mass center'''
-        raise NotImplementedError()
 
+    @abstractmethod
+    def draw(self, canvas: Canvas):
+        '''draw a shape on a tkinter canvas'''
+
+    @abstractmethod
     def __str__(self):
-        raise NotImplementedError()
+        pass
 
 class Circle(Shape):
-    '''define Circle subclass'''
-    def __init__(self, radius: float, circle_center: tuple):
+    '''Circle shape'''
+    def __init__(self, radius: float, circle_center: Point):
         self.radius = radius
         self._center = circle_center
 
     def area(self) -> float:
         return math.pi * self.radius**2
 
-    def center(self) -> tuple:
+    def center(self) -> Point:
         return self._center
 
     def name(self) -> str:
         return "Circle"
 
+    def draw(self, canvas: Canvas):
+        canvas.create_oval(
+            self._center.x - self.radius, self._center.y - self.radius,
+            self._center.x + self.radius, self._center.y + self.radius,
+            fill= "yellow", outline= "purple" )
+
     def __str__(self):
         return f"{self.name()}(center={self._center}, radius={self.radius}"
 
 class Triangle(Shape):
-    '''define Triangle subclass'''
-    def __init__(self, a: tuple, b: tuple, c: tuple):
+    '''Triangle shape'''
+    def __init__(self, a: Point, b: Point, c: Point):
         self.a = a
         self.b = b
         self.c = c
 
-    def center(self) -> tuple:
+    def center(self) -> Point:
         '''Calculate the center (centroid) of the triangle.'''
-        x = (self.a[0] + self.b[0] + self.c[0]) / 3
-        y = (self.a[1] + self.b[1] + self.c[1]) / 3
-        return x, y
+        x = (self.a.x + self.b.x + self.c.x) / 3
+        y = (self.a.y + self.b.y + self.c.y) / 3
+        return Point(x, y)
 
     def sides(self):
         '''Calculate the lengths of the sides of a triangle given its vertices.'''
@@ -69,6 +88,10 @@ class Triangle(Shape):
         triangle_area = math.sqrt(s * (s - a) * (s - b) * (s - c))
         return triangle_area
 
+    def draw(self, canvas: Canvas):
+        canvas.create_polygon(self.a.x, self.a.y, self.b.x, self.b.y, self.c.x, self.c.y,
+                              fill= "green", width=5)
+
     def name(self) -> str:
         return "Triangle"
 
@@ -76,7 +99,7 @@ class Triangle(Shape):
         return f"{self.name()}({self.a}, {self.b}, {self.c})"
 
 class Rectangle(Shape):
-    '''define Rectangle subclass'''
+    '''Rectangle shape'''
     def __init__(self, top: int, left: int, width: int, height: int):
         self.left = left
         self.top = top
@@ -86,31 +109,63 @@ class Rectangle(Shape):
     def area(self) -> float:
         return self.width * self.height
 
-    def center(self) -> tuple:
-        return (self.height/2, self.width/2)
+    def center(self) -> Point:
+        return Point(self.height/2, self.width/2)
 
     def name(self) -> str:
         return "Rectangle"
 
+    def draw(self, canvas: Canvas):
+        bottom_right_x = self.left + self.width
+        bottom_right_y = self.top + self.height
+        canvas.create_rectangle(self.left, self.top, bottom_right_x, bottom_right_y,
+                                fill= "red", outline= "white")
+
     def __str__(self):
         return f"{self.name()}(top={self.top}, left={self.left}, width={self.width}, height={self.height})"
 
+
 class Line(Shape):
-    '''define Line subclass'''
-    def __init__(self, endpoint_a: tuple, endpoint_b: tuple):
+    '''Line shape'''
+    def __init__(self, endpoint_a: Point, endpoint_b: Point):
         self.a = endpoint_a
         self.b = endpoint_b
 
     def area(self) -> float:
         return 0.0
 
-    def center(self) -> tuple:
-        x = (self.a[0] + self.b[0]) / 2
-        y = (self.b[1] + self.b[1]) / 2
-        return (x, y)
+    def center(self) -> Point:
+        x = (self.a.x + self.b.x) / 2
+        y = (self.b.y + self.b.y) / 2
+        return Point(x, y)
 
     def name(self) -> str:
         return "Line"
 
+    def draw(self, canvas: Canvas):
+        canvas.create_line(self.a.x, self.a.y, self.b.x, self.b.y, fill="purple", width=6)
+
     def __str__(self):
         return f"{self.name()}({self.a}, {self.b})"
+
+
+
+master = Tk()
+CANVAS_WIDTH = 400
+CANVAS_HEIGHT = 300
+
+canvas_widget = Canvas(master,
+                width=CANVAS_WIDTH,
+                height=CANVAS_HEIGHT)
+canvas_widget.pack()
+
+shapes = [
+    Line(Point(20, 35), Point(50, 50)),
+    Line(Point(40, 35), Point(200, 50)),
+    Rectangle(50, 20, 150, 80),
+    Circle(50, Point(150, 75)),
+    Triangle(Point(245, 200), Point(270, 50), Point(200, 90))
+]
+for shape in shapes:
+    shape.draw(canvas_widget)
+mainloop()
