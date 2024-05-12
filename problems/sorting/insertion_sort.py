39d00f3f7fa92b3d22ace7a93c8e7a26c2271de4
marklim-cs <135713612+marklim-cs@users.noreply.github.com>
2024-05-12 16:10:52 +0200

    Update problems/sorting/insertion_sort.py
    Co-authored-by: Denis Daletski <DaletskiDenis@gmail.com>

--- problems/sorting/insertion_sort.py
+++ problems/sorting/insertion_sort.py
@@ -4,7 +4,7 @@ def insertion_sort(arr: list):
     for i, key in enumerate(arr[1:], 1)
         previous_index = i - 1
 
-        while previous_index >=0 and key < arr[previous_index]:
+        while previous_index >= 0 and key < arr[previous_index]:
             arr[previous_index+1] = arr[previous_index]
             previous_index -= 1
 
