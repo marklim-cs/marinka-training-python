4ab78bfa0ba0f55216b9a27befabc65fc5ca1f8d
Marina <climovicimarina@gmail.com>
2024-06-20 19:08:15 +0200

    added classes ArrayMap, TreeMap, PythinDict

---
+++ problems/dict_implementations/test_arraymap.py
@@ -0,0 +1,20 @@
+import unittest
+from .compare_dicts import ArrayMap
+
+class TestArrayMap(unittest.TestCase):
+    def test_insert(self):
+        arr = ArrayMap()
+        arr.insert(1, "apple")
+        arr.insert(2, "banana")
+        self.assertEqual(arr.repres(), [(1, "apple"), (2, "banana")])
+        arr.insert(2, "kiwi")
+        self.assertEqual(arr.repres(), [(1, "apple"), (2, "kiwi")])
+        self.assertEqual(arr.__len__(), 2)
+    def test_find_len(self):
+        arr = ArrayMap()
+        arr.insert(1, "apple")
+        arr.insert(2, "banana")
+        self.assertEqual(arr.find(2), (2, "banana"))
+
+if __name__ == '__main__':
+    unittest.main()
