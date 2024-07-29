67714550decba159475e525b98345d44a94d13d7
Denis Dalecki <daletskidenis@gmail.com>
2024-07-29 12:30:30 +0200

    Dictionary.items() method

--- problems/dict_implementations/cli.py
+++ problems/dict_implementations/cli.py
@@ -1,7 +1,7 @@
 import argparse
 import timeit
 from pathlib import Path
-from typing import Callable
+from typing import Callable, Iterable, Tuple
 import abc
 import json
 
@@ -15,7 +15,7 @@ class Dictionary(abc.ABC):
         pass
 
     @abc.abstractmethod
-    def repres(self) -> str:
+    def items(self) -> Iterable[Tuple[object, object]]:
         pass
 
 class Node:
@@ -75,11 +75,6 @@ class TreeMap(Dictionary):
         count += self._len_recursively(node.rightchild)
         return count
 
-    def repres(self):
-        tree = self._iter_recursively(self._root)
-        list_tree = list(tree)
-        return json.dumps(list_tree, indent=2)
-
     def _iter_recursively(self, node):
         if node is None:
             return
@@ -92,6 +87,9 @@ class TreeMap(Dictionary):
         if node.rightchild is not None:
             yield from self._iter_recursively(node.rightchild)
 
+    def items(self):
+        return [*self._iter_recursively(self._root)]
+
 class PythonDict(Dictionary):
     def __init__(self):
         self.dict = {}
@@ -102,8 +100,8 @@ class PythonDict(Dictionary):
     def find(self, key):
         return self.dict.get(key)
 
-    def repres(self):
-        return json.dumps(self.dict, indent=2)
+    def items(self):
+        return self.dict.items()
 
 class ArrayMap(Dictionary):
     def __init__(self):
@@ -131,9 +129,8 @@ class ArrayMap(Dictionary):
         else:
             raise ValueError("size doesn't match, the ArrayMap is not build properly")
 
-    def repres(self):
-        result = "\n".join(f"{key} : {value}" for key, value in zip(self.keys, self.values))
-        return result
+    def items(self):
+        return zip(self.keys, self.values)
 
 def create_storage(storage_type):
     if storage_type == "tree":
@@ -156,10 +153,11 @@ def count_words(storage_factory: Callable[[], Dictionary], text_path, result_pat
         value = dictionary.find(word.lower()) or 0
         dictionary.insert(word.lower(), value + 1)
 
-    representation = dictionary.repres()
+    items = dictionary.items()
 
     with open(result_path, "w", encoding="utf-8") as fr:
-        fr.write(representation)
+        for k, v in items:
+            fr.write(f"{k}: {v}\n")
 
 
 def execution_time(storage, text_path, result_path):
@@ -184,9 +182,6 @@ def main():
         raise SystemExit(1)
 
     result_path = Path(arguments.pathresult)
-    if not result_path.exists():
-        print("The target file doesn't exist")
-        raise SystemExit(1)
 
     output = execution_time(arguments.storage, text_path, result_path)
     print(f"Time taken: {output}")
